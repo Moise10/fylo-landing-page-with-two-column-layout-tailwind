e94f2f9182457d2800fa52dc4d7ef3c0d8f42078
moise <mbakopmoise237@gmail.com>
2022-05-15 10:28:33 +0800

    working on the media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  content: ["./dist/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./dist/index.html'],
+	theme: {
+		extend: {
+			screens: {
+				sm: '360px',
+				// => @media (min-width: 640px) { ... }
+
+				md: '799px',
+				// => @media (min-width: 768px) { ... }
+
+				lg: '1024px',
+				// => @media (min-width: 1024px) { ... }
+
+				xl: '1280px',
+				// => @media (min-width: 1280px) { ... }
+
+				'2xl': '1536px',
+				// => @media (min-width: 1536px) { ... }
+			},
+			boxShadow: {
+				'3xl':'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px',
+			},
+		},
+	},
+	plugins: [],
+};
